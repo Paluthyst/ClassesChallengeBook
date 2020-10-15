3f27cdc1c48defb8af4f2fe0dfa430cac0e3b160
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-10-14 22:29:00 -0400

    Add project files.

---
+++ Challenge2MoreClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Challenge2MoreClasses
+{
+    class Keyboards
+    {
+        public string brand = "Black Web";
+        public void type()
+        {
+            Console.WriteLine("Click Clack");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Keyboards myKeyboard = new Keyboards();
+            myKeyboard.type();
+            Console.WriteLine(myKeyboard.brand + " Keyboards are good");
+        }
+    }
+}
